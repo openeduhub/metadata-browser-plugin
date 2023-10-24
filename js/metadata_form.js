9ec1a07a13800c9fe08c0192d63493f540d76ae9
Andrés Granados <afgranadosc@unal.edu.co>
2023-10-24 14:03:20 +0200

    Loaded DOM event and new response handler from web service

--- js/metadata_form.js
+++ js/metadata_form.js
@@ -1,13 +1,62 @@
-document.getElementById("testbutton").addEventListener("click", function() {
+window.addEventListener("DOMContentLoaded", (event) => {
+    document.getElementById("testbutton").addEventListener("click", function() {
 
-    var active_url = getCurrentTab();
-    
-    active_url.then(function(result) {
-        document.getElementById("url_id").value = result;
-        myFunction();
+        var active_url = getCurrentTab();
+        
+        active_url.then(function(result) {
+            document.getElementById("url_id").value = result;
+            myFunction();
+        });
     });
 
-    
+    const ul = document.querySelector("ul"),
+    input = document.querySelector("input"),
+    tagNumb = document.querySelector(".details span");
+
+    let maxTags = 10,
+    tags = ["test_tag1", "test_tag2"];
+    countTags();
+    createTag();
+    function countTags(){
+        input.focus();
+        tagNumb.innerText = maxTags - tags.length;
+    }
+    function createTag(){
+        ul.querySelectorAll("li").forEach(li => li.remove());
+        tags.slice().reverse().forEach(tag =>{
+            let liTag = `<li>${tag} <i class="uit uit-multiply" onclick="remove(this, '${tag}')"></i></li>`;
+            ul.insertAdjacentHTML("afterbegin", liTag);
+        });
+        countTags();
+    }
+    function remove(element, tag){
+        let index  = tags.indexOf(tag);
+        tags = [...tags.slice(0, index), ...tags.slice(index + 1)];
+        element.parentElement.remove();
+        countTags();
+    }
+    function addTag(e){
+        if(e.key == "Enter"){
+            let tag = e.target.value.replace(/\s+/g, ' ');
+            if(tag.length > 1 && !tags.includes(tag)){
+                if(tags.length < 10){
+                    tag.split(',').forEach(tag => {
+                        tags.push(tag);
+                        createTag();
+                    });
+                }
+            }
+            e.target.value = "";
+        }
+    }
+    input.addEventListener("keyup", addTag);
+    const removeBtn = document.querySelector(".details button");
+    removeBtn.addEventListener("click", () =>{
+        tags.length = 0;
+        ul.querySelectorAll("li").forEach(li => li.remove());
+        countTags();
+    });
+
 });
 
 
@@ -36,7 +85,6 @@ function getCurrentURL(tab){
 
 function myFunction() {
     local_url = document.getElementById("url_id").value;
-    //local_url = "https://www.weltderphysik.de/gebiet/teilchen/quanteneffekte/";
     fetch("http://127.0.0.1:5500/metadata", {
         method: 'post',
         body: JSON.stringify({ 
@@ -48,22 +96,19 @@ function myFunction() {
         }
     }).then((response) => {
         return response.json()
-    }).then((res) => {
-        let ojson = res.resultjson;
-        ojson = ojson.replace(/'/g, '"');
-        ojson = JSON.parse(ojson);
-        
+    }).then((res) => {        
+        let ojson = res;
+
         document.getElementById("title_id").value = ojson.title;
         document.getElementById("description_id").value = ojson.description;
-        document.getElementById("discipline_id").value = ojson.valuespaces.discipline;
-        document.getElementById("educontext_id").value = ojson.valuespaces.educationalContext;
-        document.getElementById("keywords_id").value = ojson.keyword;
-        document.getElementById("license_id").value = ojson.license.author;
-        if (res.status === 200) {
-
-            console.log("Post successfully created!")
-        }
+        document.getElementById("discipline_id").value = ojson.disciplines;
+        document.getElementById("educontext_id").value = ojson.educational_context;
+        document.getElementById("keywords_id").value = ojson.keywords;
+        document.getElementById("license_id").value = ojson.license;
+        document.getElementById("license_author_id").value = ojson.license_author;
+        document.getElementById("lrt_id").value = ojson.new_lrt;
     }).catch((error) => {
         console.log(error)
     });
 }
+
